a3ac1dc9df926d94b462e2548a316c54e9e2e450
ajay <1990.ajay@gmail.com>
2018-07-31 07:30:26 -0700

    Day 2 exercises done - Section7 completed

--- udemy_cpp_excercises/simple_functions.cpp
+++ udemy_cpp_excercises/simple_functions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-
+#include <algorithm>
+#include <array>
 using namespace std;
 
 
@@ -8,12 +9,19 @@ class Calculator
 private:
 	int m_x;
 	int m_y;
+	std::array <double,10>  m_arr;
 
 public:
 	//Constructor
 	Calculator(int x, int y) {
 	m_x = x;
 	m_y = y;
+	//Initialization list for m_arr
+	// m_arr  = {10,20,30,40,50,60,70,80,90,100};
+	for(int i = 0 ; i < 10 ; i++ ) {
+	cout << "Enter " << i << "th number" << endl; 
+		cin >> m_arr[i];
+	}
 	}
 	
 	//Member functions goes here 
@@ -24,13 +32,28 @@ public:
 	int subtraction() {
 		return (m_x - m_y);
 	}
+	void sort() {
+		std::sort(m_arr.begin(), m_arr.end());
+		cout << " MinValue" << m_arr[0] << endl;
+		cout << "Max Value" << *(m_arr.begin()) << endl;
+	}
 
 };
+
+int add(int x, int y=0 , int z=0) {
+	return (x+y+z);
+	}
 
 int main() {
 	Calculator calc(20,10);
 	cout << "Result of addition = " << calc.addition() << endl;
 	cout << " Result of subtraction = " << calc.subtraction();
+	// calc.sort(); 
+	int x = 10;
+	cout << " Adding only 1 number " << add(x) << endl;
+	int y = 20;
+	cout << " Adding only 2 number " << add(x,y) << endl;
+	
 	return 0;
 	}
 
